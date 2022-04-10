c43d40aa586dd24ff14e6886d028f36ad2999a23
Artem Anashkin <artem1anashkin@gmail.com>
2022-04-11 01:11:06 +0300

    add favicon and update certificates code

--- src/components/NavBar/navigations/Certificates/Certificates.tsx
+++ src/components/NavBar/navigations/Certificates/Certificates.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import tms from '../../../../image/Certificates/tms.jpg'
+import udemyru from '../../../../image/Certificates/udemy_ru.png'
+import udemyen from '../../../../image/Certificates/udemy_en.png'
+
+import styles from './certificates.module.scss'
 
 const Certificates = () => {
   return (
     <>
-        <div>Certificates</div>
+        <div className={styles.certificates}>
+          <div className={styles.certificates__wrapper}>
+            <div className={styles.certificates__item}>
+              <div className={styles.item__img}>
+                <img src={tms} alt="tms" />
+              </div>
+            </div>
+            <div className={styles.certificates__item}>
+              <div className={styles.item__img}>
+                <img src={udemyru} alt="udemyru" />
+              </div>
+            </div>
+            <div className={styles.certificates__item}>
+              <div className={styles.item__img}>
+                <img src={udemyen} alt="udemyen" />
+              </div>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
