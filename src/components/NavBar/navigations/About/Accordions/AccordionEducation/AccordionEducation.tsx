c05991cba29af33408313cef69e3030336de43f7
Artem Anashkin <artem1anashkin@gmail.com>
2022-04-13 12:28:15 +0300

    update accordion

--- src/components/NavBar/navigations/About/Accordions/AccordionEducation/AccordionEducation.tsx
+++ src/components/NavBar/navigations/About/Accordions/AccordionEducation/AccordionEducation.tsx
@@ -1,22 +1,54 @@
-import React, {useState} from 'react'
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import React, {useEffect, useRef, useState} from 'react';
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai';
+import { IoIosSchool } from "react-icons/io";
 
 import styles from '../accordion.module.scss'
 
 const AccordionEducation = () => {
     const [selected, setSelected] = useState(false)
+    const [height, setHeight] = useState('');
+
+    const refHeigth = useRef<HTMLDivElement>(null);
+       
+    useEffect(() => {
+       return(
+         setHeight(`${refHeigth.current?.scrollHeight}px`)
+       )
+    }, [])
+   
+    const handleClick = () => {
+      setSelected(!selected)
+    }
   
   return (
     <div className={styles.accordion}>
         <div className={styles.accordion__wrapper}>
-             <div className={styles.title__wrapper} onClick={() => setSelected(!selected)}>
+             <div className={styles.title__wrapper} onClick={handleClick}>
                 <div className={styles.title}>Education</div>
-                <div className={styles.plus} onClick={() => setSelected(!selected)}>{selected ? <AiOutlineMinus /> : <AiOutlinePlus />}</div>
+                <div className={styles.plus}>{selected ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />}</div>
              </div>
-             {
-             selected && 
-             <div className={styles.content}>Education</div>
-             }
+
+             <div className={selected ? styles.animated : styles.content}
+                  style={{minHeight: selected ? `${height}` : "0px"}}
+                  ref={refHeigth}
+            >
+               <div className={styles.icon}><IoIosSchool /></div>
+               <div className={styles.item}>
+                 <div className={styles.item__institution}>BSMTC</div>
+                 <div className={styles.item__descr}>Bobruisk State Mechanical and Technological College - Technician technologist</div>
+                 <div className={styles.item__descr}>2011-2015</div>
+               </div>
+               <div className={styles.item}>
+                 <div className={styles.item__institution}>UDEMY</div>
+                 <div className={styles.item__descr}>Online course - WEB Developer</div>
+                 <div className={styles.item__descr}>2021</div>
+               </div>
+               <div className={styles.item}>
+                 <div className={styles.item__institution}>TeachMeSkills</div>
+                 <div className={styles.item__descr}>Offline course - Frond End Developer</div>
+                 <div className={styles.item__descr}>2021-2022</div>
+               </div>
+             </div>
         </div>
         
     </div>
--- src/components/NavBar/navigations/About/Accordions/AccordionWork/AccordionWork.tsx
+++ src/components/NavBar/navigations/About/Accordions/AccordionWork/AccordionWork.tsx
@@ -1,23 +1,53 @@
-import React, { useState } from 'react'
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import React, { useEffect, useState, useRef } from 'react'
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
+import { FaHandsHelping } from 'react-icons/fa'
 
 import styles from '../accordion.module.scss'
 
-const AccordionWork = () => {
+
+const AccordionWork:React.FC = () => {
 
  const [selected, setSelected] = useState(false)
+ const [height, setHeight] = useState('');
+
+ const refHeigth = useRef<HTMLDivElement>(null);
+    
+ useEffect(() => {
+    return(
+      setHeight(`${refHeigth.current?.scrollHeight}px`)
+    )
+ }, [])
+
+ const handleClick = () => {
+   setSelected(!selected)
+ }
 
   return (
     <div className={styles.accordion}>
         <div className={styles.accordion__wrapper}>
-            <div className={styles.title__wrapper} onClick={() => setSelected(!selected)}>
+            <div className={styles.title__wrapper} onClick={handleClick}>
                 <div className={styles.title}>WORK EXPERIENCE</div>
-                <div className={styles.plus} onClick={() => setSelected(!selected)}>{selected ? <AiOutlineMinus /> : <AiOutlinePlus />}</div>
+                <div className={styles.plus}>{selected ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />}</div>
             </div>
-            {
-            selected && 
-            <div className={styles.content}>Work</div>
-            }
+
+            <div               
+              className={selected ? styles.animated : styles.content}
+              style={{minHeight: selected ? `${height}` : "0px"}}
+              ref={refHeigth}              
+            >
+              <div className={styles.icon}><FaHandsHelping /></div>
+              <div className={styles.item}>
+                 <div className={styles.item__institution}>Karavaeshnikov</div>
+                 <div className={styles.item__descr}>Individual entrepreneur - Assistant manager</div>
+                 <div className={styles.item__descr}>2018-2020</div>
+              </div>
+              <div className={styles.item}>
+                 <div className={styles.item__institution}>ZapadKhimTorg</div>
+                 <div className={styles.item__descr}>Private trading unitary enterprise - Driver</div>
+                 <div className={styles.item__descr}>2020-2022</div>
+               </div>
+            </div>
+
         </div>    
     </div>
   )
